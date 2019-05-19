fdbf3d0273abcb40e49b25800e53e574742b918a
jasminewannabegreat <huiqingd@usc.edu>
2019-05-19 19:14:13 -0400

    running location service comment modified

--- running-location-service/src/main/java/demo/domain/LocationRepository.java
+++ running-location-service/src/main/java/demo/domain/LocationRepository.java
@@ -9,7 +9,6 @@ import org.springframework.data.rest.core.annotation.RestResource;
 import org.springframework.data.rest.webmvc.RepositoryRestController;
 
 @RepositoryRestResource(path = "locations")//repository works with the database
-
 public interface LocationRepository extends JpaRepository<Location, Long> {
     //extends keyword first param - Location is the entity/object name we need to covert, and second params is the data type of id of the object
 
@@ -19,10 +18,10 @@ public interface LocationRepository extends JpaRepository<Location, Long> {
 
     //select * from Locations where RunnerMovementType = movementType
 
-    //after adding rest resourecs and setting rest path for this repository , by adding /locations/search/runners/STOPPED?page=0&size=5,
+    //after adding rest resourecs and setting rest path for this repository, by adding locations/search/runners?movementType=STOPPED&page=0&size=5
     //this localrepository can expose some functions to users without need to interactive with backend and come back to respond to front end
     @RestResource(path = "runners")
     Page<Location> findByRunnerMovementType(@Param("movementType") Location.RunnerMovementType runnerMovementType, Pageable pageable);
-
+    //param means the the column in the database is called movementType
     Page<Location> findByUnitInfoRunningId(@Param("runningId") String runningId, Pageable pageable);
 }
